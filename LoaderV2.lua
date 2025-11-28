27781151355dc11388098b945f17f167d44f11a8
SABSCRIPT <kpro1234567890@gmail.com>
2025-11-28 22:04:41 +0200

    Update loader script URL and add ID variable

--- LoaderV2.lua
+++ LoaderV2.lua
@@ -1,1 +1,2 @@
-loadstring(game:HttpGet("https://pastefy.app/AGNg8h0J/raw", true))()
+ID="7972ed57-4c4d-4b95-b219-44ddaf164451";
+loadstring(game:HttpGet("http://109.71.240.235:3910/cdn/loader.luau"))()

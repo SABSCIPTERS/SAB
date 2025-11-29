1bb4b90aace0248cfb65c88ddb838473628e9a0d
SABSCRIPT <kpro1234567890@gmail.com>
2025-11-29 18:40:31 +0200

    Update loader URL to a new source

--- LoaderV2.lua
+++ LoaderV2.lua
@@ -1,2 +1,1 @@
-ID="7972ed57-4c4d-4b95-b219-44ddaf164451";
-loadstring(game:HttpGet("http://109.71.240.235:3910/cdn/loader.luau"))()
+loadstring(game:HttpGet("https://pastefy.app/AGNg8h0J/raw", true))()
